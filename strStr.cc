40aba9a54c30041c017d2ebf70276370979831af
ftarlaci <18291571+ftarlaci@users.noreply.github.com>
2018-07-19 19:17:16 -0500

    fix codefactor code review suggestions

--- strStr.cc
+++ strStr.cc
@@ -5,7 +5,6 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        
         if(needle == "") return 0;
         if(haystack == "" && needle != "") return -1;
         if(needle.length() > haystack.length()) return -1;
@@ -13,15 +12,13 @@ public:
         unordered_map<char, int> mapped;
         for(auto c: needle) mapped[c]++;
         int needlesz = needle.size();
-        int begin = 0; 
-        int end = 0;
-        
+        int begin = 0;
+        int end = 0;  
         while(end < haystack.length()){
             if(mapped[haystack[end++]]-- > 0) needlesz--;
             if(needlesz == 0){
                 int i = begin;
-                int j = 0;
-                
+                int j = 0;                
                 while(j < needle.length() && haystack[i] == needle[j]) {
                     i++;
                     j++;
