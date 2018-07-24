da7777b7f57c93a13015d238d967268a2ae37090
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-24 15:54:58 -0500

    last word length

---
+++ LengthOfLastWord.cc
@@ -0,0 +1,21 @@
+// Given a string s consists of upper/lower-case alphabets and empty
+// space characters ' ', return the length of last word in the string.
+// If the last word does not exist, return 0.
+// Note: A word is defined as a character sequence consists of non-space 
+// characters only.
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+    	if(s == " ") return 0; // string is empty
+        char space = ' ';
+    	int i = s.length() - 1;
+        while(i >= 0 && s[i] == space) {
+            i--;
+        }
+        int j = i;
+        while(j >= 0 && s[j] != space){
+            j--;           
+        }
+        return i - j;
+    }
+};
