44e935a4f54a6401308de3cf095c297ae3b1bc88
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-25 22:19:12 -0500

    add binary

---
+++ addBinary.cc
@@ -0,0 +1,8 @@
+// Given two binary strings, return their sum (also a binary string).
+// The input strings are both non-empty and contains only characters 1 or 0.
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        
+    }
+};
