ca7085689a15bc299dbed7056f628a6e514d2b40
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-27 20:56:06 -0500

    remove duplicates from linked list, my favorite thing.

---
+++ removeDuplicatesSortedList.cc
@@ -0,0 +1,15 @@
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+    }
+};
