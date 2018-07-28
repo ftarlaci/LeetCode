5699a5007a8526c1a248562007cd2f076386c6e6
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-28 17:43:31 -0500

    removing duplicates: sorted/unsorted linked list

--- removeDuplicatesSortedList.cc
+++ removeDuplicatesSortedList.cc
@@ -10,6 +10,30 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
+    	Listnode *curr = head;
+    	while(curr->next != nullptr){
+    		if(curr->val == curr->next->val){
+    			curr->next = curr->next->next;
+    		}
+    		curr = curr->next;
+    	}
+};
+// I went ahead and dealt with the duplicates in an unsorted linked list as well. 
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+    	Listnode *temp = head->next;
+    	Listnode *prev;
+    	Listnode *nextOfnext;
+    	while(temp->next != nullptr){
+    		prev->next = temp->next;
+    		nextOfnext = temp;
+    		temp = temp->next;
+    		if()
+    	}
+
+
         
     }
 };
