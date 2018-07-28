75bcf398f1d614459682a2d89423085060f8c7a3
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-27 20:51:10 -0500

    climbing stairs, best couple: memoize $ recurse

--- climbingStairs.cc
+++ climbingStairs.cc
@@ -3,7 +3,15 @@
 // Note: Given n will be a positive integer.
 class Solution {
 public:
+    unordered_map<int, int> cache;
     int climbStairs(int n) {
-        
+		if(cache.find(n)!= cache.end()) return cache[n];
+		int output = 0;
+		if (n == 0) result = 0;
+		else if (n == 1) output = 1;
+		else if (n == 2) output = 2;
+		else output = climbStairs(n - 1) + climbStairs(n - 2);
+		cache[n] = output;
+		return output;
     }
 };
