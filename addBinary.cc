59633cf946e0eb73f559e59fc9d75b890ac4214b
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-27 19:58:43 -0500

    added binary

--- addBinary.cc
+++ addBinary.cc
@@ -3,6 +3,25 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
+    	string sum = "";
+    	int i = a.size() - 1;
+    	int j = b.size() - 1;
+    	int carry = 0;
+    	while(i >= 0 || j >= 0 || carry == 1){
+    		carry +=((i >=0) ? a[i] - '0' : 0);
+    		carry +=((j >=0) ? b[j] - '0' : 0);
+    		sum = char(carry % 2 + '0') + sum;
+    		// check for carry
+    		carry /= 2;
+    		i--;
+    		j--;
+    	}
+    		return sum;
+    } 
+}
+
+
+    	
         
     }
 };
