9863ce640a174acaa36bfa904f92497e33a18ef5
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-29 22:00:26 -0500

    remove duplicates take II

--- removeDuplicatesSortedList.cc
+++ removeDuplicatesSortedList.cc
@@ -26,6 +26,32 @@ public:
         return head;
     }
 };
+
+
+// 82. Remove Duplicates from Sorted List II
+//Given a sorted linked list, delete all nodes
+//that have duplicate numbers, leaving only distinct numbers from the original list.
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+    	if(head == nullptr) return nullptr;
+        ListNode *curr = head;
+        ListNode *temp;
+    	while(curr->next != nullptr){
+            //check if curr->next->next is not null first.
+    		if(curr->val == curr->next->val){
+    			temp = curr->next->next;
+                delete curr->next;
+                curr->next = temp;
+            }else {
+                curr = curr->next;
+            }
+        }
+        return head;
+
+        
+    }
+};
 // I will go ahead and dealt with the duplicates in an unsorted linked list as well. 
 // Given a sorted linked list, delete all duplicates such that each element appear only once.
 class Solution {
