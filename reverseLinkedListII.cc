61e92f729996073686c1d3863ea568c99e6741e2
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-30 22:25:26 -0500

    More linked list fun

---
+++ reverseLinkedListII.cc
@@ -0,0 +1,16 @@
+// Reverse a linked list from position m to n. Do it in one-pass.
+// Note: 1 ≤ m ≤ n ≤ length of list.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        
+    }
+};
