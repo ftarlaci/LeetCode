cd737c475ae0a29a2c9daed0d3714e6408995cd1
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-08-11 19:33:44 -0500

    from sorted vector to BST

--- convertSortedArrayToBST.cc
+++ convertSortedArrayToBST.cc
@@ -20,6 +20,19 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        
+    	//if vector is empty, return null. 
+    	if(nums.size() == 0) return nullptr;
+    	
+    	// create a new node that points to the middle of the vector. 
+    	TreeNode* root = new TreeNode(nums[nums.size() / 2]);
+    	
+    	// create two vectors as from the main vector by dividing in the middle.  
+    	vector<int> leftVec(nums.begin(), nums.begin() + nums.size() / 2 + 1);
+    	vector<int> rightVec(nums.begin() + nums.size() / 2 + 1, nums.end());
+    	
+    	// point the root pointer to the subvectors [0] and recurse on both sub-vectors. 
+    	root->leftVec = sortedArrayToBST(leftVec);
+    	root->rightVec = sortedArrayToBST(rightVec);
+    	return root; 
     }
 };
