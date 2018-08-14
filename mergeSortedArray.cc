8693e99171a1ba9a78d62d9912e017c1502860fd
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-08-13 22:53:16 -0500

    merge arrays

--- mergeSortedArray.cc
+++ mergeSortedArray.cc
@@ -15,6 +15,11 @@ Output: [1,2,2,3,5,6]
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+       int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (j >= 0) {
+            nums1[k--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        } 
     }
 };
