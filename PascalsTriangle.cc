ab1cd88e7d9f36dde131df3e736dbb4b40f30dfe
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-08-19 10:14:29 -0500

    pascal's

--- PascalsTriangle.cc
+++ PascalsTriangle.cc
@@ -2,7 +2,14 @@
 // In Pascal's triangle, each number is the sum of the two numbers directly above it.
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        
+    vector<vector<int>> generate(int numRows) {vector<vector<int>> out(numRows);
+        for (int i = 0; i < numRows; i++) {
+            out[i].resize(i + 1);
+            out[i][0] = out[i][i] = 1;
+            for (int j = 1; j < i; j++){
+                out[i][j] = out[i - 1][j - 1] + out[i - 1][j];
+            }
+        }   
+        return out; 
     }
 };
