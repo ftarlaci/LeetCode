0584ccc2a190c8b722b034261d5319bff2b8854a
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-08-20 19:46:59 -0500

    linked list palindrome

---
+++ palindromeLinkedList.cc
@@ -0,0 +1,22 @@
+/*Given a singly linked list, determine if it is a palindrome.
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+    }
+};
