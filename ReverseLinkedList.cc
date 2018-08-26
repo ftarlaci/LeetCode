01d7cf262005f29742d1eb945fd993bb7c514e73
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-08-26 18:40:09 -0500

    binary tree tilt

--- ReverseLinkedList.cc
+++ ReverseLinkedList.cc
@@ -1,10 +1,10 @@
 /**206. Reverse Linked List
-Reverse a singly linked list.
-Example:
-Input: 1->2->3->4->5->NULL
-Output: 5->4->3->2->1->NULL
-Follow up:
-A linked list can be reversed either iteratively or recursively. Could you implement both?
+	Reverse a singly linked list.
+	Example:
+	Input: 1->2->3->4->5->NULL
+	Output: 5->4->3->2->1->NULL
+	Follow up:
+	A linked list can be reversed either iteratively or recursively. Could you implement both?
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -16,8 +16,8 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
     	if(!head || head->next == nullptr) return head;
-        ListNode* prev = head;
-        ListNode* curr = prev->next;
+        ListNode* prev = head; // prev now points to wherever "head" is pointing to.
+        ListNode* curr = prev->next; 
         while (curr) {
             prev->next = curr->next;
             curr->next = head;
--- reverseLinkedListII.cc
+++ reverseLinkedListII.cc
@@ -1,6 +1,10 @@
 // Reverse a linked list from position m to n. Do it in one-pass.
 // Note: 1 ≤ m ≤ n ≤ length of list.
 /**
+	Example:
+	Input: 1->2->3->4->5->NULL, m = 2, n = 4
+	Output: 1->4->3->2->5->NULL
+	
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -10,6 +14,7 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int m, int n) {     
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+
     }
 };
