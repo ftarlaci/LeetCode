7c6675d16af5b1822993405876d1f42c714c7825
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-08-27 07:21:31 -0500

    reverse linked list from m to n

--- reverseLinkedListII.cc
+++ reverseLinkedListII.cc
@@ -4,7 +4,7 @@
 	Example:
 	Input: 1->2->3->4->5->NULL, m = 2, n = 4
 	Output: 1->4->3->2->5->NULL
-	
+
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -15,6 +15,22 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
-
+    	ListNode* left = new ListNode(0);
+        left->next = head;
+        int x = m; 
+        int y = n - m;
+        while(--x) left = left->next;
+        ListNode* prev = left->next;
+        ListNode* curr = prev->next;
+        ListNode* next;
+        while(y--){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        left->next->next = curr;
+        left->next = prev;
+        return m == 1 ? prev : head;
     }
 };
