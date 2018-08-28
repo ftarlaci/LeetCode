47f74fb77df651beec54432dafce0ee5ab613c0e
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-08-28 09:13:07 -0500

    various

--- mergeKSortedLists.cc
+++ mergeKSortedLists.cc
@@ -1,14 +1,15 @@
-/**Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
-Example:
-Input:
-[
-  1->4->5,
-  1->3->4,
-  2->6
-]
-Output: 1->1->2->3->4->4->5->6
-* Definition for singly-linked list.
- * struct ListNode {
+/**	Merge k sorted linked lists and return it as one sorted list. 
+	Analyze and describe its complexity.
+	Example:
+	Input:
+	[
+	  1->4->5,
+	  1->3->4,
+	  2->6
+	]
+	Output: 1->1->2->3->4->4->5->6
+ *  Definition for singly-linked list.
+ *  struct ListNode {
  *     int val;
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
@@ -17,5 +18,6 @@ Output: 1->1->2->3->4->4->5->6
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+    	
     }
 };
