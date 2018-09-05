25446517f3f754df5c32097ba6ed189967dedc7b
ftarlaci <18291571+ftarlaci@users.noreply.github.com>
2018-09-04 21:59:59 -0500

    Update firstUniqueCharacterInString.cc

--- firstUniqueCharacterInString.cc
+++ firstUniqueCharacterInString.cc
@@ -1,17 +1,17 @@
 /*	
-	Given a string, find the first non-repeating character in it and
-	return it's index. If it doesn't exist, return -1.
-	Examples:
-	s = "leetcode"
-	return 0.
-	s = "loveleetcode",
-	return 2.
-	Note: You may assume the string contain only lowercase letters.
+Given a string, find the first non-repeating character in it and
+return it's index. If it doesn't exist, return -1.
+Examples:
+s = "leetcode"
+return 0.
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
 */
 class Solution {
 public:
     int firstUniqChar(string s) {
-    	vector<int> vec(26, 0);
+    	vector<int> vec(26, 0);  // give a size to the vector at initialization and set each index to 0
         for (auto uniq : s) {
         	vec[uniq - 'a'] += 1;
         }
