8b4cdd33396a84f49fc25eadc3d83b18008b6533
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-09-12 22:16:05 -0500

    majority element done

--- majorityElement.cc
+++ majorityElement.cc
@@ -15,6 +15,33 @@ Output: 2
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+    	unordered_map<int, int> freqMap; 
+        int n = nums.size();
+        for (int i = 0; i < n; i++){
+            if (++freqMap[nums[i]] > n / 2){
+            	return nums[i];
+            }
+         }
+          
+
+          // below is the first (unsuccessful) attempt, keeping it for comparison
+
+        // int sz = nums.size();
+        // if(sz == 2) return nums[0]; // guaranteed to have a majority element so both of the numbers have to be the same. 
+        // int majority = floor(sz / 2); 
+        // unordered_map<int, int> freqMap;
         
+        // // If you don't want to change the items as well as want to avoid making 
+        // // copies, then auto const & is the correct choice // stackoverflow
+        // for(auto const & i : nums){
+        //     freqMap[nums[i]]++; // store nums and frequency in the map[i]
+        // }
+        
+        // for(unordered_map<int, int>::iterator itr = freqMap.begin(); itr != freqMap.end(); ++itr){
+        //     if(itr->second > majority){
+        //         return itr->first;
+        //     }
+        // }
+        // return 0;
     }
 };
