284c1db3b6d13ff8f4d4fe0924d9e36beb496843
Fatma <coderphd@gmail.com>
2018-10-11 17:30:47 -0500

    is power of two

---
+++ powerOf3.cc
@@ -0,0 +1,18 @@
+/*
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+*/
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;   // 3^19 biggest power fo 3
+    }
+};
